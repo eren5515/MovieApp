942cea1f118d5bf10784cfd8ad73b7544fa6273b
eren5515 <eren1256@gmail.com>
2024-05-10 15:34:06 +0300

    fix:fixed cdn problem

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -15,7 +15,7 @@ export default function Index() {
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
-      <div className="about-right"><img src="https://cdn.discordapp.com/attachments/336171716126572548/1237913754226786375/Elegant_Beige_Black_Film_Maker_Cinematography_Business_Logo__2_-removebg-preview.png?ex=663d6086&is=663c0f06&hm=b6c420327aa4f14e9f1b4783b40357f44decc50c97695586e042c7af3d2b0906&" alt="" /></div>
+      <div className="about-right"><img src="https://raw.githubusercontent.com/eren5515/asd/main/Elegant_Beige_Black_Film_Maker_Cinematography_Business_Logo__2_-removebg-preview.png" alt="" /></div>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <header>
       <Link href="/">
         <img
-          src="https://cdn.discordapp.com/attachments/336171716126572548/1237913754226786375/Elegant_Beige_Black_Film_Maker_Cinematography_Business_Logo__2_-removebg-preview.png?ex=663d6086&is=663c0f06&hm=b6c420327aa4f14e9f1b4783b40357f44decc50c97695586e042c7af3d2b0906&"
+          src="https://raw.githubusercontent.com/eren5515/asd/main/Elegant_Beige_Black_Film_Maker_Cinematography_Business_Logo__2_-removebg-preview.png"
           alt=""
         />
       </Link>
